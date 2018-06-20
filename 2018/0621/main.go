42401c581c46b7bdd77010a7d05fd4f169b979be
ktshun <ktshun1027@yahoo.co.jp>
2018-06-21 02:10:34 +0900

    # 非同期処理とファイル処理2
    * 読み込みRoutineから受け取った値をさらに別Routineで処理させる

--- 2018/0621/main.go
+++ 2018/0621/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -22,12 +24,20 @@ func main() {
 	}
 
 	queue := make(chan string, 10)
+	waitGroup := new(sync.WaitGroup)
 
 	go readFile(fp, queue)
 
 	for i := range queue {
-		fmt.Println(i)
+		waitGroup.Add(1)
+		go waitAndPrintStr(i, waitGroup)
 	}
+
+	fmt.Println("読み込みは終わったよ")
+
+	waitGroup.Wait()
+
+	fmt.Println("表示まで終わったよ")
 }
 
 /**
@@ -42,4 +52,13 @@ func readFile(fp *os.File, queue chan string) {
 	for scanner.Scan() {
 		queue <- scanner.Text()
 	}
+}
+
+/**
+ * 100ms待って標準出力に出力
+ */
+func waitAndPrintStr(s string, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
+	time.Sleep(time.Millisecond * 1000)
+	fmt.Println(s)
 }
